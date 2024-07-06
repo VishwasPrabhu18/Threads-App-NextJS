ea2477af5899a59ece0a4e5367e53171113085d4
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-06 21:06:32 +0530

    update with clerk sign-in and sign-up

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from 'next/font/google';
+import "../globals.css";
 
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
+export const methadata = {
+  title: "Threads",
+  desription: "A Next.js 14 Meta Threads Application"
 }
 
-export default layout
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout ({
+  children
+}: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${inter.className} bg-dark-1`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
+    <>
+      <h1 className='head-text text-left'>Home</h1>
+    </>
   )
 }
 
-export default Home
+export default Home;
