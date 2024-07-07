0e00778571789e216bc60b6f2de3fded19ddb73d
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-07 19:22:26 +0530

    update

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -19,21 +19,19 @@ const LeftSidebar = () => {
             const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
 
             return (
-              <div>
-                <Link
-                  href={link.route}
-                  key={link.label}
-                  className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
-                >
-                  <Image
-                    src={link.imgURL}
-                    alt={link.label}
-                    width={24}
-                    height={24}
-                  />
-                  <p className='text-light-1 max-lg:hidden'>{link.label}</p>
-                </Link>
-              </div>
+              <Link
+                href={link.route}
+                key={link.label}
+                className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+              >
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  width={24}
+                  height={24}
+                />
+                <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+              </Link>
             )
           })
         }
