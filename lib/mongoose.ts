8dda3e2a92d228da476c512fac88c37fcd05336b
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-07 19:20:16 +0530

    mongodb connection is added

---
+++ lib/mongoose.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
+  
+  if(isConnected) {
+    console.log("Already connected to DB");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true;
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log("Error connecting to DB", error);    
+  }
+}
